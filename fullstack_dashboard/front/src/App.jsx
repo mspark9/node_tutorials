a2bcfd6354b14c6c4d1780a0c883a95275c0d87d
mspark9 <stellaaa0906@gmail.com>
2026-01-26 17:06:39 +0900

    initial setting

---
+++ fullstack_dashboard/front/src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/visitors')
+      .then(response => {
+        setData(response.data); // 서버에서 받은 데이터를 상태에 저장
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  console.log(data)
+
+  return (
+    <>
+      <h2>Data from server</h2>
+      {
+        data.map((a, b) => (
+          <div key={b}>
+            <h2>{a.month}</h2>
+            <p>{a.update_date}</p>
+          </div>
+        )
+        )
+      }
+    </>
+  )
+}
+
+export default App
