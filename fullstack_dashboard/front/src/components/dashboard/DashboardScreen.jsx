9d02844e8c4399f9d589049cd950453b3a3cad0f
mspark9 <stellaaa0906@gmail.com>
2026-01-27 18:20:08 +0900

    dashboard contents imcomplete

--- fullstack_dashboard/front/src/components/dashboard/DashboardScreen.jsx
+++ fullstack_dashboard/front/src/components/dashboard/DashboardScreen.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Sales from './Sales'
+import Visitors from './Visitors'
 
 const DashboardScreen = () => {
     return (
-        <div>DashboardScreen</div>
+        <div className='w-[calc(80%-28px)] ml-[calc(20%+14px)] mt-[14px]'>
+            <div className='grid grid-cols-[4fr_3fr] gap-x-[14px]'>
+                <Sales />
+                <Visitors />
+            </div>
+        </div>
     )
 }
 
--- fullstack_dashboard/front/src/components/dashboard/Visitors.jsx
+++ fullstack_dashboard/front/src/components/dashboard/Visitors.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchVisitors } from '../../redux/slices/apiSlice';
+import HeadTitle from './HeadTitle';
 
 const Visitors = () => {
 
@@ -14,7 +15,9 @@ const Visitors = () => {
     // console.log(state)
 
     return (
-        <div>Visitors</div>
+        <div className='block-wrap'>
+            <HeadTitle title="Visitors Insights" />
+        </div>
     )
 }
 
